fecd942a772f691487c75a0bb2eaf912ee4224ef
Sophie Miller <millersg47@gmail.com>
2022-07-19 17:50:08 -0700

    adds github and stackoverflow to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import linkedin from "../images/linkedin.png";
 import email from "../images/email.png";
+import github from "../images/github-logo.png";
+import stackoverflow from "../images/stackoverflow.png";
 import "../styles/footer.css";
 
 function Footer() {
@@ -11,6 +13,12 @@ function Footer() {
       </a>
       <a href="mailto: millersg47@gmail.com">
         <img className="icon" src={email} />
+      </a>
+      <a href="https://github.com/millersg47">
+        <img className="icon" src={github} />
+      </a>
+      <a href="https://stackoverflow.com/users/17968918/sophiem">
+        <img className="icon" src={stackoverflow} />
       </a>
     </div>
   );
